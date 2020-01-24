b3c0bd237dab67dd266fece9d473cfc44f96cdf5
Myunghwan Kim <doctork200@gmail.com>
2020-01-24 23:29:57 +0000

    feat: Add Read function in ApiController class

--- src/main/java/com/samkim/study/springboot/web/PostsApiController.java
+++ src/main/java/com/samkim/study/springboot/web/PostsApiController.java
@@ -1,6 +1,7 @@
 package com.samkim.study.springboot.web;
 
 import com.samkim.study.springboot.service.posts.PostsService;
+import com.samkim.study.springboot.web.dto.PostsResponseDto;
 import com.samkim.study.springboot.web.dto.PostsSaveRequestDto;
 import com.samkim.study.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
@@ -20,5 +21,10 @@ public class PostsApiController {
     @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
