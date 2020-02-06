34c0aa93c894f49e55c20670f3f2617fad2f8abb
Myunghwan Kim <doctork200@gmail.com>
2020-02-06 20:01:18 +0000

    delete function 서비스 메소드 추가

--- src/main/java/com/samkim/study/springboot/service/posts/PostsService.java
+++ src/main/java/com/samkim/study/springboot/service/posts/PostsService.java
@@ -47,4 +47,12 @@ public class PostsService {
                 .map(PostsListResponseDto::new) // 람다식 : .map(posts -> new PostsListResponseDto(posts))
                 .collect(Collectors.toList());
     }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
 }
