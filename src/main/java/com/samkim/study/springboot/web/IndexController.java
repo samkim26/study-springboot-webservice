cd2067e00df02948bd6b6d38ce68e7f30ad5520c
Myunghwan Kim <doctork200@gmail.com>
2020-02-06 20:03:21 +0000

    서비스에서 만든 delete 메소드 컨트롤러가 사용하도록 코드 추가

--- src/main/java/com/samkim/study/springboot/web/IndexController.java
+++ src/main/java/com/samkim/study/springboot/web/IndexController.java
@@ -5,6 +5,7 @@ import com.samkim.study.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -31,5 +32,12 @@ public class IndexController {
         model.addAttribute("post", dto);
 
         return "posts-update";
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+
+        return id;
     }
 }
