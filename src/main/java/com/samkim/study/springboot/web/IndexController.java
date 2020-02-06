3f6252dbe12005f2219197995f32987c0187083e
Myunghwan Kim <doctork200@gmail.com>
2020-02-06 20:22:43 +0000

    서비스에서 만든 delete 메소드 컨트롤러가 사용하도록 하는 코드 수정(컨트롤러 수정)

--- src/main/java/com/samkim/study/springboot/web/IndexController.java
+++ src/main/java/com/samkim/study/springboot/web/IndexController.java
@@ -33,11 +33,4 @@ public class IndexController {
 
         return "posts-update";
     }
-
-    @DeleteMapping("/api/v1/posts/{id}")
-    public Long delete(@PathVariable Long id) {
-        postsService.delete(id);
-
-        return id;
-    }
 }
