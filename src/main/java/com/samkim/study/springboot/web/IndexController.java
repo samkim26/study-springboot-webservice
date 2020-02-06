1d3a1da3d04f97abb7735d5d7323f9a67e742b86
Myunghwan Kim <doctork200@gmail.com>
2020-02-06 00:24:18 +0000

    Controller 코드 변경 : Model을 사용하여 결과를 index.mustache에 전달

--- src/main/java/com/samkim/study/springboot/web/IndexController.java
+++ src/main/java/com/samkim/study/springboot/web/IndexController.java
@@ -1,14 +1,25 @@
 package com.samkim.study.springboot.web;
 
+import com.samkim.study.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) { // Model : 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음. 여기서는 결과를 posts로 index.mustache 에 전달함
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
 }
