a12450810c410b04c280d31c40eeeb558296508c
Myunghwan Kim <doctork200@gmail.com>
2020-02-18 00:33:03 +0000

    index.mustache에서 userName을 사용할 수 있게 userName을 model에 저장하는 코드 추가

--- src/main/java/com/samkim/study/springboot/web/IndexController.java
+++ src/main/java/com/samkim/study/springboot/web/IndexController.java
@@ -1,23 +1,32 @@
 package com.samkim.study.springboot.web;
 
+import com.samkim.study.springboot.config.auth.LoginUser;
+import com.samkim.study.springboot.config.auth.dto.SessionUser;
 import com.samkim.study.springboot.service.posts.PostsService;
 import com.samkim.study.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) { // Model : 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음. 여기서는 결과를 posts로 index.mustache 에 전달함
+    public String index(Model model, @LoginUser SessionUser user) { // Model : 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음. 여기서는 결과를 posts로 index.mustache 에 전달함
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
